acf9e6824f6f2e65ea0d57297851bf3dc7870b05
yyhhga <woohaa3630@hotmail.com>
2024-08-07 11:14:10 +0800

    update test config for @ alias

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
    plugins: [react()],
+   resolve: {
+      alias: {
+         //add alias for imports with @/
+         '@': path.resolve(__dirname, './src'),
+      },
+   },
    test: {
       environment: 'jsdom',
    },
