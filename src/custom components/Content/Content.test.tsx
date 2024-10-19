e73ef7e4751ef562dcf16a3b3619d17a21f5fc8e
yyhhga <woohaa3630@hotmail.com>
2024-10-19 12:05:55 +0800

    cleanup packages and files

--- src/custom components/Content/Content.test.tsx
+++
@@ -1,8 +0,0 @@
-import { assert, test } from 'vitest'
-import { render } from '@testing-library/react'
-import { Content, ContentType } from '.'
-
-test('Page', () => {
-   render(<Content type={ContentType.SIMPLE} />)
-   assert(true, 'this is a dummy test')
-})
